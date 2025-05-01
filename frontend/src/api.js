5fbcb7a0e7f03cbf96a2439fc85bd98773e9e054
Bhaskar Saini <bhaskarsaini333@gmail.com>
2025-05-01 23:09:40 +0530

    api js

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
-const api = axios.create({
-  baseURL: import.meta.env.VITE_API_BASE_URL
-});
+const baseURL =
+  process.env.NODE_ENV === 'development'
+    ? 'http://localhost:5000/api'
+    : 'https://neversolo.onrender.com/api';
 
 export default api;
